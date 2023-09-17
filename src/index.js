d4582f1761ca70461db0febf50a580c0ee236572
scherpablo <scherpablo@gmail.com>
2023-09-17 14:51:04 -0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const http = require("http");
 const url = require("url");
-const fs = require("fs");
 const data = require("./utils/data");
 
 const PORT = 4001;
@@ -8,15 +7,15 @@ const PORT = 4001;
 const server = http.createServer((req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
 
-  const { pathname } = url.parse(req.url);
+  const { pathname } = url.parse(req.url);//Extraemos el pathname de la url
 
-  if (pathname.startsWith("/rickandmorty/character")) {
-    const parts = pathname.split("/");
+  if (pathname.startsWith("/rickandmorty/character")) {//Verificamos si la ruta comienza con...
+    const parts = pathname.split("/");//Dividimos la ruta en partes mediante / y creamos un array con cada parte de la ruta
     const characterId = parts[3]; // El id está en la posición 3
 
     // Buscar el personaje por ID en data.js
-    const character = data.find(
-      (character) => character.id === parseInt(characterId)
+    const character = data.find(//Buscamos el id dentro de data
+      (character) => character.id === parseInt(characterId)//Convertomos el string a number
     );
 
     if (character) {
